52a8e01e96efef05b20da8604537297fe6459ae5
alfinabdullah15 <alfin.abdullah@gmail.com>
2020-02-08 22:30:04 +0700

    mvc category

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    //INI ADALAH METHOD UNTUK MENG-HANDLE RELATIONSHIPS
+    public function parent()
+    {
+        //KARENA RELASINYA DENGAN DIRINYA SENDIRI, MAKA CLASS MODEL DIDALM belongsTo() ADALAH NAMA CLASSNYA SENDIRI YAKNI CATEGORY
+        //belongsTo DIGUNAKAN UNTUK REFLEKSI KE DATA INDUKNYA
+        return $this->belongsTo(Category::class);
+    }
+
+    //UNTUK LOCAL SCOPE NAMA METHODNYA DIAWAL DENGAN KATA scope DAN DIIKUTI DENGAN NAMA METHOD YANG DIINGINKAN
+    //CONTOH: scopeNamaMethod()
+    //Local scope digunakan untuk mengelompokkan query yang bisa digunakan kembali pada kondisi lain 
+    public function scopeGetParent($query)
+    {
+        //SEMUA QUERY YANG MENGGUNAKAN LOCAL SCOPE INI AKAN SECARA OTOMATIS DITAMBAHKAN KONDISI whereNul('parent_id')
+        return $query->whereNull('parent_id');
+    }
 }
